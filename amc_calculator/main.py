e9af7a254c4d9c326fe4404d6459e2839399bb54
gem-subhash <121420581+gem-subhash@users.noreply.github.com>
2023-02-22 18:15:08 +0530

    Update main.py

--- amc_calculator/main.py
+++ amc_calculator/main.py
@@ -7,11 +7,13 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-
+import os
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount('/static', StaticFiles(directory=os.path.join(os.getcwd(), 'static')), name='static')
+
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
